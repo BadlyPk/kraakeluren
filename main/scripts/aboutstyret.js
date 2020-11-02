86cf9e5bda4d024315487513245b6ba8467293d9
justiceforpeanuts <adriansafari@yahoo.no>
2020-11-03 00:31:15 +0100

    oppdatert styret

--- main/scripts/aboutstyret.js
+++ main/scripts/aboutstyret.js
@@ -2,11 +2,22 @@ function showimage(imgs) {
   var expandImg = document.getElementById("expandedImg");
   var imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
-  expandImg.parentElement.style.display = "block";
-  var imags = document.getElementsByTagName("img");
   var i;
-  for (i=0; i<imags.length-1; i++){
-    imags[i].style.visibility="hidden";
-  }
-  imgText.parentElement.style.visibility= "visible";
+  var galcolumn = document.getElementsByClassName('galcolumn');
+  for (var i = 0; i < galcolumn.length; ++i) {
+      galcolumn[i].style.visibility="hidden";
+    }
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display="block";
+}
+
+function backtogal(){
+  var oldgallery = document.getElementsByClassName('galcolumn');
+  for (var i = 0; i < oldgallery.length; ++i) {
+      oldgallery[i].style.visibility="visible";
+    }
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.parentElement.style.display="none";
+  imgText.parentElement.style.display="none";
 }
