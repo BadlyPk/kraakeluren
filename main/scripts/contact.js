6d7ab2ec2478bb296c0435d13a23afa75b7776e3
justiceforpeanuts <adriansafari@yahoo.no>
2020-11-03 20:24:21 +0100

    mazemap update

--- main/scripts/contact.js
+++ main/scripts/contact.js
@@ -1,17 +1,17 @@
-var map = new map.Map({
-    // container id specified in the HTML
-    container: 'map',
+var map = new Mazemap.Map({
+// container id specified in the HTML
+container: 'map',
 
-    campuses: 121,
+campuses: 121,
 
-    // initial position in lngLat format
-    center: {lng: 13.270286316716465, lat: 52.502217640505705},
+// initial position in lngLat format
+center: {lng: 13.270286316716465, lat: 52.502217640505705},
 
-    // initial zoom
-    zoom: 18,
+// initial zoom
+zoom: 18,
 
-    zLevel: 3
+zLevel: 3
 });
 
 // Add zoom and rotation controls to the map.
-map.addControl(new mazemap.mapboxgl.NavigationControl());
+map.addControl(new Mazemap.mapboxgl.NavigationControl());
