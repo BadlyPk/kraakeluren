6b1b9c8196158d8885de4772340bc76089b23bad
justiceforpeanuts <adriansafari@yahoo.no>
2020-11-03 02:23:10 +0100

    aboutstyret oppdatering

--- main/scripts/aboutstyret.js
+++ main/scripts/aboutstyret.js
@@ -2,13 +2,18 @@ function showimage(imgs) {
   var expandImg = document.getElementById("expandedImg");
   var imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
+  expandImg.parentElement.style.display="block";
   var i;
   var galcolumn = document.getElementsByClassName('galcolumn');
   for (var i = 0; i < galcolumn.length; ++i) {
       galcolumn[i].style.visibility="hidden";
     }
-  imgText.innerHTML = imgs.alt;
-  expandImg.parentElement.style.display="block";
+  var bildetxt=document.getElementsByTagName("p")
+  for (var i = 0; i < bildetxt.length; ++i) {
+      if(imgs.alt==bildetxt[i].id){
+        imgText.innerHTML = imgs.alt +"<br />" +"<br />"+bildetxt[i].textContent
+      }
+    }
 }
 
 function backtogal(){
