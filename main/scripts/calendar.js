1dcdb97b89a740ac82ac65c4e5d5a441e0b40da9
Lenny-B <spillformeg@live.no>
2020-11-04 22:44:26 +0100

    almost there

--- main/scripts/calendar.js
+++ main/scripts/calendar.js
@@ -1,36 +1,49 @@
 var current = new Date();
 var currentYear = current.getFullYear();
-var currentMonth = current.getMonth();
-var currentDay = current.getDay();
+var currentMonth = current.getMonth() + 1;
+var currentDate = current.getDate();
+
+var y = currentYear;
+var m = currentMonth;
+
 const months = ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"];
-
 document.getElementById("year").innerHTML = currentYear;
-document.getElementById("month").innerHTML = months[currentMonth];
+document.getElementById("month").innerHTML = months[currentMonth - 1];
 
+//events format:
+//yr: yyyy
+//mo: mm
+//da: d / dd
+//event: Kort beskrivelse av aktivitet
+//description: lang beskrivelse av aktivitet
 var events = [
 {
   yr: 2020,
   mo: 10,
   da: 24,
-  event: "Lorem ipsum"
+  event: "Lorem ipsum",
+  description: ""
 },
 {
   yr: 2020,
   mo: 11,
-  da: 12,
-  event: "Ut auctor malesuada molestie."
+  da: 4,
+  event: "Ut auctor malesuada molestie.",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus commodo accumsan. Maecenas condimentum aliquet mollis. Phasellus nec dolor eget sem aliquet tincidunt placerat vitae mi. Maecenas consectetur nulla non metus tempus sodales. Phasellus dapibus at nunc tincidunt eleifend. Phasellus ut tortor vel urna egestas gravida ut vitae est. Proin."
 },
 {
   yr: 2019,
   mo: 11,
   da: 20,
-  event: "Lorem ipsum"
+  event: "Lorem ipsum",
+  description: ""
 },
 {
   yr: 2020,
   mo: 11,
   da: 29,
-  event: "Quisque ullamcorper lacus vel magna."
+  event: "Quisque ullamcorper lacus vel magna.",
+  description: ""
 }
 ];
 
@@ -46,26 +59,26 @@ function isLeap(y){
 }
 
 
-function createCalendar(y, m){
-  var timestampString = (m + 1).toString() + " " + "01" + " " + y.toString();
+function createCalendar(){
+  var timestampString = m.toString() + " " + "01" + " " + y.toString();
   var timestamp = Date.parse(timestampString);
   var d = new Date(timestamp);
   var fdom = (d.getDay() + 6) % 7;  //First day of month, 0=Mon 1=Tue, 2=Wed ...
   var ldom = 0;                     //Last date of month
 
-  if (m == 1){                      //Update ldom based on month and leapyear
+  if (m == 2){                      //Update ldom based on month and leapyear
     if (isLeap(y)){
       ldom = 29;
     } else {
       ldom = 28;
     }
-  } else if (m < 7 && m % 2 === 0 || m > 6 && m % 2 === 1){
+  } else if (m < 8 && m % 2 === 1 || m > 7 && m % 2 === 0){
     ldom = 31;
   } else {
     ldom = 30;
   }
 
-  var eiM = events.filter(e => e.yr === y && e.mo === (m + 1));   //Find all events in this month and year
+  var eiM = events.filter(e => e.yr === y && e.mo === m);   //Find all events in this month and year
 
   var i = 1;
   while(i <= ldom){
@@ -77,6 +90,11 @@ function createCalendar(y, m){
       var cdb = document.createElement("div");
       cdb.className = "calendarDateBox";
       if (j == fdom && i <= ldom){
+        if (y === currentYear && m === currentMonth && i === currentDate){
+          cdb.style.backgroundColor = "#AABBFF";
+          cdb.style.border = "2px solid #000000"
+        }
+
         var db = document.createElement("div");
         db.className = "dateBox";
         db.innerHTML = i.toString();
@@ -84,6 +102,8 @@ function createCalendar(y, m){
 
         var ed = eiM.find(e => e.da === i);
         if (ed != undefined){
+          cdb.style.cursor = "pointer";
+          cdb.setAttribute("onclick","popUp(this);");
           var eb = document.createElement("div");
           eb.className = "eventBox";
           eb.innerHTML = ed.event;
@@ -99,4 +119,63 @@ function createCalendar(y, m){
   }
 }
 
-createCalendar(currentYear, currentMonth);
+function nextMonth(){
+  if (m === 12){
+    m = 1;
+    y++;
+    document.getElementById("year").innerHTML = y;
+  } else {
+    m++;
+  }
+  document.getElementById("month").innerHTML = months[m - 1];
+  document.getElementById("calendarContainer").innerHTML = "";
+  createCalendar();
+}
+
+function prevMonth(){
+  if (m === 1){
+    m = 12;
+    y--;
+    document.getElementById("year").innerHTML = y;
+  } else {
+    m--;
+  }
+  document.getElementById("month").innerHTML = months[m - 1];
+  document.getElementById("calendarContainer").innerHTML = "";
+  createCalendar();
+}
+
+function popUp(box){
+  var cover = document.createElement("div");
+  cover.id = "screenCover";
+  var popup = document.createElement("div");
+  popup.className = "calendarDescription";
+  var title = document.createElement("div");
+  title.className = "fixedRow";
+  title.style.justifyContent = "space-between";
+  var d = box.children[0].innerHTML;
+  var titleEvent = events.find(e => e.yr === y && e.mo === m && e.da == d);
+
+  var h = document.createElement("H2");
+  h.style.margin = "15px 10px 1em 10px";
+  var t = "";
+  if (parseInt(d) < 10){
+    t = "0";
+  }
+  var t = t + d + "/" + titleEvent.mo + "-" + titleEvent.yr + ": " + titleEvent.event;
+  h.appendChild(document.createTextNode(t));
+  title.appendChild(h);
+
+  var i = document.createElement("I");
+  i.className = "fa fa-times fa-3x cross";
+  i.onclick = function(){
+    document.getElementById("screenCover").remove();
+  }
+  title.appendChild(i);
+
+  popup.appendChild(title);
+  cover.appendChild(popup);
+  document.body.appendChild(cover);
+}
+
+createCalendar();
