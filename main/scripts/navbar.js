ee42314da4d5b204e87ba6172db7292d87369b42
BadlyPk <martin.pham10@yahoo.no>
2020-11-04 01:03:45 +0100

    TIng

--- main/scripts/navbar.js
+++ main/scripts/navbar.js
@@ -1,16 +1,33 @@
 //From https://www.w3schools.com/howto/howto_js_navbar_shrink_scroll.asp (with some of our changes)
-
-window.onscroll = function() {
-  scrollFunction()
-};
-function scrollFunction() {
-  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
-  document.getElementById("navbar").style.padding = "20px 10px";
-  document.getElementById("Logo").style.width = '90px';
-  document.getElementById("Logo").style.height = '90px';
+function scrollFunction2() {
+  if(window.innerWidth < 960) {
+      document.getElementById("navbar").style.padding = "10px 5px";
   } else {
-  document.getElementById("navbar").style.padding = "40px 10px";
-  document.getElementById("Logo").style.width = '150px';
-  document.getElementById("Logo").style.height = '150px';
+    window.onscroll = function() {
+      scrollFunction()
+    };
+    document.getElementById("navbar").style.padding = "40px 10px";
+    document.getElementById("Logo").style.width = '150px';
+    document.getElementById("Logo").style.height = '150px';
+  }
+}
+
+scrollFunction2()
+
+window.onresize = function() {
+    scrollFunction2()
+};
+
+function scrollFunction() {
+  if(window.innerWidth < 960) {
+      document.getElementById("navbar").style.padding = "10px 5px";
+  } else if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("Logo").style.width = '90px';
+    document.getElementById("Logo").style.height = '90px';
+  } else {
+    document.getElementById("navbar").style.padding = "40px 10px";
+    document.getElementById("Logo").style.width = '150px';
+    document.getElementById("Logo").style.height = '150px';
   }
 }
