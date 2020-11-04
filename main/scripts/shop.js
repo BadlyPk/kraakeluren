43ead8da92863c11577357b25e0c7531a5127f5a
cathrinebecker <33757769+cathrinebecker@users.noreply.github.com>
2020-11-04 21:16:37 +0100

    Update shop.js

--- main/scripts/shop.js
+++ main/scripts/shop.js
@@ -1,3 +1,6 @@
+/* This is a quite simple function to "buy" items. The code lets the user add to cart, view the cart and order. To order the user have to enter their email-address. In reality we would then want the person who manages the shop to get an email about the order, and he/she will see the email address the items are ordered from so he/she can contact the buyer about payment/delivery. This is outside the curriculum, but we have implemented almost the full code; we store the values (number of items and which ones, and email address) in an array without sending the information further. */
+
+/* An empty cart to add items to later */
 var handlekurv = [
   ["Lerke", 0],
   ["Pin", 0],
@@ -5,11 +8,15 @@ var handlekurv = [
   ["Epost", 0]
 ]
 
+/* The number of different items in the cart */
 var lerkeNum = 0;
 var pinNum = 0;
 var daljeNum = 0;
+
+/* Variable for the price so that we can display it for the user */
 var pris = 0;
 
+/* Get the empty paragraphs and divs, to add stuff later */
 var lerkeT = document.getElementById("lerkeT");
 var pinT = document.getElementById("pinT");
 var daljeT = document.getElementById("daljeT");
@@ -18,6 +25,8 @@ var prisT = document.getElementById("totalPris");
 var textBox = document.getElementById("textBox");
 var kontaktInfo = document.getElementById("kontaktInfo");
 var takk = document.getElementById("takk");
+
+/* The following code decides what happens when the uses clicks to add an item to cart */
 
 var lerke = document.getElementById("lerke");
 lerke.addEventListener("click", function() {
@@ -52,16 +61,38 @@ dalje.addEventListener("click", function() {
   prisT.innerHTML = "Totalpris: " + pris + "kr";
 });
 
+/* Set all values to 0 and remove all blocks if the user wants to empty the cart */
 var slettBestilling = document.getElementById("slettBestilling");
 slettBestilling.addEventListener("click", function() {
-  location.reload();
+  textBox.style.display = "none";
+  takk.style.display = "none";
+  kontaktInfo.style.display = "none";
+  validateTekst.style.display = "none";
+
+  lerkeNum = 0;
+  pinNum = 0;
+  daljeNum = 0;
+  pris = 0;
+
+  lerkeT.innerHTML = "Lerke: " + lerkeNum;
+  pinT.innerHTML = "Pin: " + pinNum;
+  daljeT.innerHTML = "Dalje: " + daljeNum;
+
+  handlekurv = [
+    ["Lerke", 0],
+    ["Pin", 0],
+    ["Dalje", 0],
+    ["Epost", 0]
+  ];
 });
 
+/* Let the user enter their email and order */
 var videre = document.getElementById("videre");
 videre.addEventListener("click", function() {
   kontaktInfo.style.display = "block";
 });
 
+/* Validate email and let user order */
 var validateTekst = document.getElementById("validateTekst");
 var bestill = document.getElementById("bestill");
 bestill.addEventListener("click", function() {
