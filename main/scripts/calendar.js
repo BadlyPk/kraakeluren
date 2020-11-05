0a624540f54df7d3757259d7f04ff3c3666ccb90
Lenny-B <spillformeg@live.no>
2020-11-05 01:28:34 +0100

    comments og litt fiks

--- main/scripts/calendar.js
+++ main/scripts/calendar.js
@@ -10,7 +10,7 @@ const months = ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "Au
 document.getElementById("year").innerHTML = currentYear;
 document.getElementById("month").innerHTML = months[currentMonth - 1];
 
-//events format:
+//Event format:
 //yr: yyyy
 //mo: mm
 //da: d / dd
@@ -96,6 +96,7 @@ var events = [
 }
 ];
 
+//Sjekker om året er skuddår
 function isLeap(y){
   if(y % 400 === 0){
     return true;
@@ -107,7 +108,7 @@ function isLeap(y){
   return false;
 }
 
-
+//Lager kalendermåned basert på globale variable y og m
 function createCalendar(){
   var timestampString = m.toString() + " " + "01" + " " + y.toString();
   var timestamp = Date.parse(timestampString);
@@ -115,7 +116,7 @@ function createCalendar(){
   var fdom = (d.getDay() + 6) % 7;  //First day of month, 0=Mon 1=Tue, 2=Wed ...
   var ldom = 0;                     //Last date of month
 
-  if (m == 2){                      //Update ldom based on month and leapyear
+  if (m == 2){                      //Oppdater ldom basert på måned og skuddår
     if (isLeap(y)){
       ldom = 29;
     } else {
@@ -127,50 +128,52 @@ function createCalendar(){
     ldom = 30;
   }
 
-  var eiM = events.filter(e => e.yr === y && e.mo === m);   //Find all events in this month and year
+  var eiM = events.filter(e => e.yr === y && e.mo === m);   //Finner alle event objects i denne måneden
 
   var i = 1;
-  while(i <= ldom){
-    var r = document.createElement("div");
+  while(i <= ldom){                                        //Itererer over alle dagene i måneden
+    var r = document.createElement("div");                 //En rad for hver uke
     r.className = "fixedRow";
 
     var j;
-    for (j = 0; j < 7; j++){
-      var cdb = document.createElement("div");
+    for (j = 0; j < 7; j++){                              //Itererer over alle dagene i en uke
+      var cdb = document.createElement("div");            //En boks for hver dato
       cdb.className = "calendarDateBox";
-      if (j == fdom && i <= ldom){
-        if (y === currentYear && m === currentMonth && i === currentDate){
+      if (j == fdom && i <= ldom){                        //Legger bare til ting i boksene etter dato stemmer med dagen den skal være på og før siste dato
+        if (y === currentYear && m === currentMonth && i === currentDate){    //Styler nådagen litt annerledes
           cdb.style.backgroundColor = "#AABBFF";
           cdb.style.border = "2px solid #000000";
           cdb.style.borderRadius = "6px";
         }
 
-        var db = document.createElement("div");
+        var db = document.createElement("div");           //Boks for datotallet
         db.className = "dateBox";
         db.innerHTML = i.toString();
         cdb.appendChild(db);
 
-        var ed = eiM.find(e => e.da === i);
+        var ed = eiM.find(e => e.da === i);               //Finner dagens event object
         if (ed != undefined){
-          cdb.style.cursor = "pointer";
+          cdb.style.cursor = "pointer";                   //Gir boks popupfunksjon og pointer pga event
           cdb.setAttribute("onclick","popUp(this);");
-          var eb = document.createElement("div");
+
+          var eb = document.createElement("div");         //Boks for kort beskrivelse av event
           eb.className = "eventBox";
           eb.innerHTML = ed.event;
           cdb.appendChild(eb);
         }
 
-        i++;
-        fdom = (fdom + 1) % 7;
+        i++;                        //Neste dag
+        fdom = (fdom + 1) % 7;      //For å holde lik med j
       }
-      r.appendChild(cdb);
+      r.appendChild(cdb);           //Legg boks til i rad
     }
-    document.getElementById("calendarContainer").appendChild(r);
+    document.getElementById("calendarContainer").appendChild(r);  //Legg til rad i kalender
   }
 }
 
+//Funksjon for å oppdatere kalender for neste måned
 function nextMonth(){
-  if (m === 12){
+  if (m === 12){        //Overflow til neste år
     m = 1;
     y++;
     document.getElementById("year").innerHTML = y;
@@ -178,10 +181,11 @@ function nextMonth(){
     m++;
   }
   document.getElementById("month").innerHTML = months[m - 1];
-  document.getElementById("calendarContainer").innerHTML = "";
-  createCalendar();
+  document.getElementById("calendarContainer").innerHTML = "";  //Fjerner forrige kalendermåned
+  createCalendar();                                             //Lager ny
 }
 
+//Funksjon for å oppdatere kalender for forrige måned
 function prevMonth(){
   if (m === 1){
     m = 12;
@@ -195,35 +199,47 @@ function prevMonth(){
   createCalendar();
 }
 
+//Funksjon for å lage popup boks når man klikker på en dato med en event
 function popUp(box){
-  var cover = document.createElement("div");
+  var cover = document.createElement("div");    //Lager div som dekker kalenderen så man ikke kan klikke på ting i kalenderen
   cover.id = "screenCover";
-  var popup = document.createElement("div");
+  cover.onmousedown = function(e){              //Fjerner popup når man klikker utfor boksen
+    if (e.target.id === "screenCover"){
+      this.remove();
+    }
+  }
+
+  var popup = document.createElement("div");    //Boks for popup
   popup.className = "calendarDescription";
-  var title = document.createElement("div");
+
+  var title = document.createElement("div");    //Boks for dato + tittel på event
   title.className = "fixedRow";
   title.style.justifyContent = "space-between";
-  var d = box.children[0].innerHTML;
-  var dayEvent = events.find(e => e.yr === y && e.mo === m && e.da == d);
 
-  var h = document.createElement("H2");
-  h.style.margin = "15px 10px 0 10px";
+  var d = box.children[0].innerHTML;            //Henter igjen dato for å finne riktig event
+  var dayEvent = events.find(e => e.yr === y && e.mo === m && e.da == d);   //Henter event objekt
+
+  var h = document.createElement("H2");         //h2 tag for dato + tittel
+  h.style.margin = "auto";
   var t = "";
-  if (parseInt(d) < 10){
+  if (parseInt(d) < 10){    //Legger på 0 før dato mellom 1-9 -> eks: 05 for den femte
     t = "0";
   }
-  var t = t + d + "/" + dayEvent.mo + "-" + dayEvent.yr + ": " + dayEvent.event;
-  h.appendChild(document.createTextNode(t));
+  var t = t + d + "/" + dayEvent.mo + "-" + dayEvent.yr;    //Fullfører date
+  h.appendChild(document.createTextNode(t));                //legger til dato
+  h.appendChild(document.createElement("BR"));              //Lineshift
+  h.appendChild(document.createTextNode(dayEvent.event))    //Tittel
   title.appendChild(h);
 
-  var i = document.createElement("I");
-  i.className = "fa fa-times fa-3x cross";
-  i.onclick = function(){
+  var i = document.createElement("I");                      //I tag for å holde kryssicon
+  i.className = "fa fa-times fa-3x cross";                  //Kryss fra font awesome
+  i.onclick = function(){                                   //Legger på onclick funksjon på kryss for å fjerne popup
     document.getElementById("screenCover").remove();
   }
   title.appendChild(i);
-  popup.appendChild(title);
+  popup.appendChild(title);                                 //Legger tittel til popupboks
 
+  //Lager boks for lengre beskrivelse av event
   var desc = document.createElement("div");
   desc.className = "fixedRow";
   var p = document.createElement("P");
@@ -232,8 +248,10 @@ function popUp(box){
   desc.appendChild(p);
   popup.appendChild(desc);
 
+  //Fest popup på cover og cover på body
   cover.appendChild(popup);
   document.body.appendChild(cover);
 }
 
+//Lager initial kalender for nådagen
 createCalendar();
